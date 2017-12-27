1c70b1d755702734e1fefc60bbc1c0d516d14a16
MEHAKMEET <mehakmeet.singh2016@vitstudent.ac.in>
2017-12-27 22:05:35 +0530

    initiated structure,API time

--- app/src/main/java/example/mehakmeet/postreqtorecyclerview/MainActivity.java
+++ app/src/main/java/example/mehakmeet/postreqtorecyclerview/MainActivity.java
@@ -2,14 +2,26 @@ package example.mehakmeet.postreqtorecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import example.mehakmeet.postreqtorecyclerview.adapter.MyAdapter;
+import example.mehakmeet.postreqtorecyclerview.model.food;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView mRecyclerView;
+    ArrayList<food> foodList=new ArrayList<>();
+    MyAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        mRecyclerView=findViewById(R.id.recyclerview);
+
+
 
     }
 }
